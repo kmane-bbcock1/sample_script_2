fb367534e1ba80d719a5f9acee67ace026c6f7f5
shiptest <shiptestlink@gmail.com>
2017-05-04 08:53:38 +0000

    echo.sh created online with Bitbucket

---
+++ echo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+echo "testing runshtrigger!!!"
+echo SAMPLE_SCRIPT=res2
+echo IS_FORK=$RES1_IS_FORK
+echo IS_PR=$RES2_IS_PULL_REQUEST
+echo REPONAME=$RES2_REPO_FULL_NAME
+echo PRREPONAME=$RES2_PULL_REQUEST_REPO_FULL_NAME
