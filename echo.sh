cf2f9f60df7178fd15f5f6d0819a8d33197a363e
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-09-27 08:21:49 +0000

    echo.sh edited online with Bitbucket

--- echo.sh
+++ echo.sh
@@ -1,7 +1,2 @@
 #!/bin/bash -e
-echo "testing runshtrigger!!!"
-echo SAMPLE_SCRIPT=res2
-echo IS_FORK=$RES2_IS_FORK
-echo IS_PR=$RES2_IS_PULL_REQUEST
-echo REPONAME=$RES2_REPO_FULL_NAME
-echo PRREPONAME=$RES2_PULL_REQUEST_REPO_FULL_NAME
+echo "testing runshtrigger for test20 series!!!"
